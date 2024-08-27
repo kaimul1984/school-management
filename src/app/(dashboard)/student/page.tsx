d0eafa1747f6b6677a260b18f0c8092aa9fc6b8f
kaimul alam <kaimuljewel@yahoo.com>
2024-08-28 04:24:41 +1000

    second commit

--- src/app/(dashboard)/student/page.tsx
+++ src/app/(dashboard)/student/page.tsx
@@ -1,7 +1,23 @@
+import Announcments from "@/components/Announcments";
+import BigCalender from "@/components/BigCalender";
+import EventCalender from "@/components/EventCalender";
 import React from "react";
 
-function StudentPage() {
-  return <div>StudentPage</div>;
+export default function StudentPage() {
+  return (
+    <div className="p-4 flex gap-4 flex-col xl:flex-row">
+      {/* LEFT */}
+      <div className="w-full xl:w-2/3">
+        <div className="h-full bg-white rounded-md p-4">
+          <h1 className="text-xl font-semibold">Schedule (4A)</h1>
+          <BigCalender />
+        </div>
+      </div>
+      {/* RIGHT */}
+      <div className="w-full xl:w-1/3 flex flex-col gap-8">
+        <EventCalender />
+        <Announcments />
+      </div>
+    </div>
+  );
 }
-
-export default StudentPage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,37 @@
+import Link from "next/link";
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="w-full py-8 flex flex-wrap items-center justify-center">
+      <div className="w-full max-w-[1420px] flex flex-wrap gap-8 mx-auto">
+        <Link
+          href="/admin"
+          className="flex-1 p-4 flex items-center justify-center bg-lamaYellow rounded-xl"
+        >
+          Admin
+        </Link>
+        <Link
+          href="/student"
+          className="flex-1 p-4 flex items-center justify-center bg-lamaYellow rounded-xl"
+        >
+          Student
+        </Link>
+        <Link
+          href="/teacher"
+          className="flex-1 p-4 flex items-center justify-center bg-lamaYellow rounded-xl"
+        >
+          Teacher
+        </Link>
+        <Link
+          href="/parent"
+          className="flex-1 p-4 flex items-center justify-center bg-lamaYellow rounded-xl"
+        >
+          Parent
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
